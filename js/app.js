00966b3e401aba8e5c74a48b720a8678a05529d4
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-08 15:00:41 +0100

    FEAT main structure, task detection.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//1 chiedere ad utente una parola
+askWord
+
+
+//2 usare parola chiesta e capire se palindroma
+  //creo array di parole palindrome
+    //confronto le parole dell'array con l'input
+
+
+//3 comunico all'utente se la parola e' palindroma oppure NO
