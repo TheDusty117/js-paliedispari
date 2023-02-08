80650d3044dc8a0cbc461b2b5fc4bb4b43f5479c
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-08 15:54:55 +0100

    FEAT adding subtasks?

--- js/app.js
+++ js/app.js
@@ -2,13 +2,42 @@ console.log('funziono')
 
 //1 chiedere ad utente una parola
 let askText = prompt('inserisci una parola').split("")
+// il testo inserito da utente diventera' un array che ha come indici le lettere
+//della parola inserita
 
-console.log(typeof askText, askText)
+console.log(typeof askText, askText, askText.length)
+
+//dividere la parola in due parti, per difetto(in modo tale che se 5 lettere)
+// comunque posso confrontare una meta' con un'altra di egual dimensione
+
+
+let wordSize = askText.length
+let wordHalfSize = Math.floor(wordSize / 2)
+
+console.log(wordHalfSize)
+
+
+
+
+
+
 
 function palindromeCheck(word) {
-  if askText[i] === 
+  return word
 }
- 
+
+//2 dividere la parola in singoli elementi, per poter fare un confronto.
+// controllo fattibile con un for???
+
+
+
+
+
+
+
+
+
+
 //SPLIT crea un array di lettere di cui la parola inserita da UTENTE e' COMPOSTA
 //ES. (OSSO   [O,S,S,O])
 
