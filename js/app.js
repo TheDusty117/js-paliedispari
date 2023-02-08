d022b5b972c15c1e0425cf42b52e66ff89b32914
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-08 15:28:04 +0100

    FEAT working on the tasks notes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
+console.log('funziono')
+
 //1 chiedere ad utente una parola
-askWord
+let askText = prompt('inserisci una parola').split("")
+
+console.log(typeof askText, askText)
+
+//SPLIT crea un array di lettere di cui la parola inserita da UTENTE e' COMPOSTA
+//ES. (OSSO   [O,S,S,O])
+
+//2 capire se la parola e' palindroma
+  //usare un ciclo for, che confronta indice per indice se la parola e' uguale???
+
+// verificare che queste parole siano uguali in ambo i sensi di lettura
 
 
-//2 usare parola chiesta e capire se palindroma
-  //creo array di parole palindrome
-    //confronto le parole dell'array con l'input
 
 
-//3 comunico all'utente se la parola e' palindroma oppure NO
+
+// con split riesco a dividere cio' che utente inserisce, e farlo diventare un array
+//SAPPIAMO CONFRONTARE DEI DATI IN UN ARRAY RISPETTO A CIO' CHE INSERISCE UTENTE?
+
+//3 con
